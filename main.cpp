fdfddef7cee2d505b1f30930a9766227177e5a5e
Duc Vu <dvu2@ut.utm.edu>
2024-09-02 16:06:54 -0500

    Sum function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
-  cout <<"Hello World" << endl;
+  int n;
+  int print = 0;
+  print = sum(n);
+  cout <<print << endl;
   
   return 0;
+}
+
+int sum(int n){
+  int total = 0;
+  for(int i =0; i<=n; i++){
+    total += i;
+  }
+  return total;
+
 }
